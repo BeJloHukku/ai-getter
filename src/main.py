cbfce65909e0aa6cd9c59a60ec82d541c5590007
BeJloHukku <igoryashamb@gmail.com>
2025-12-11 15:30:12 +0300

    feat: created func for adding data to datebase ans setted up the endpoints for this

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI, Body, Request
+from typing import Annotated, AsyncGenerator
+from fastapi import FastAPI, Body, Request, Depends
 from contextlib import asynccontextmanager
 
-from src.database.crud import get_user_requests
+from src.database.crud import add_request_data, get_user_requests
 from src.database.db import engine
 from src.database.models import Base
 from src.gemini_client import get_answer_from_gemini
+from src.database.db import new_session
+
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 @asynccontextmanager
@@ -13,21 +17,35 @@ async def lifespan(app: FastAPI):
         await conn.run_sync(Base.metadata.create_all)
     yield
 
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with new_session() as session:
+        yield session
+
 
 app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/requests")
-async def get_my_requests(request: Request):
+async def get_my_requests(
+    request: Request,
+    session: Annotated[AsyncSession, Depends(get_session)]
+):
     ip_address = request.client.host
     print(f"{ip_address=}")
-    user_requests = await get_user_requests(ip_address=ip_address)
+    user_requests = await get_user_requests(
+        ip_address=ip_address, 
+        session=session
+    )
     return user_requests
 
 
 @app.post("/requests")
 async def send_prompt(
-        prompt: str = Body(embed=True),
+        request: Request,
+        prompt: Annotated[str, Body(embed=True)],
+        session: Annotated[AsyncSession, Depends(get_session)]
 ):
-    answer = await get_answer_from_gemini(prompt)
-    return {"data": answer}
+    ip_address = request.client.host
+    response = await get_answer_from_gemini(prompt)
+    await add_request_data(ip_address, prompt, response, session)
+    return {"data": response}
