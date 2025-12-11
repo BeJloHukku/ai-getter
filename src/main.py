4f4bfb920c5634c50416a37d0c018d0d331758ff
BeJloHukku <igoryashamb@yandex.ru>
2025-12-11 13:00:57 +0300

    feat: added the lifespan for creating db tables before starting api

--- src/main.py
+++ src/main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI, Body
+from contextlib import asynccontextmanager
 
+from src.db import engine
+from src.models import Base
 from src.gemini_client import get_answer_from_gemini
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/requests")
 async def get_my_requests():
     return "Hello world"
+
 
 @app.post("/requests")
 async def send_prompt(
